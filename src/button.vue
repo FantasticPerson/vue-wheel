c292e49970bf58c9e3df80cf2f8406e4068f6a5e
wdd119 <1240172019@qq.com>
2018-11-18 23:07:48 +0800

    为button添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,19 +1,40 @@
 <template>
-    <button class="g-button">hi</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon"class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
-
+    props:{
+        icon:{},
+        iconPosition:{
+            type:String,
+            default:'left',
+            validator(value){
+                return value === 'right' || value === 'left'
+            }
+        }
+    }
 }
 </script>
 <style lang="scss">
 .g-button {
+  padding: 0 1em;
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -23,6 +44,26 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+  > .icon {
+    vertical-align: middle;
   }
 }
 </style>
