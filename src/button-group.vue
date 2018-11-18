8094e40c924999621fe566cc08fc4a222011484f
wdd119 <1240172019@qq.com>
2018-11-18 23:48:13 +0800

    fix bug：如果 group-button 的 里面的 button  不是第一个  不需要 margin-left:-1

--- src/button-group.vue
+++ src/button-group.vue
@@ -16,24 +16,26 @@ export default {
 }
 </script>
 <style lang="scss">
-    .g-button-group{
-        display: inline-flex;
-        vertical-align: middle;
-        >.g-button{
-            border-radius: 0;
-            margin-left: -1px;
-            &:first-child{
-                border-top-left-radius: var(--border-radius);
-                border-bottom-left-radius: var(--border-radius);
-            }
-            &:last-child{
-                border-top-right-radius: var(--border-radius);
-                border-bottom-right-radius: var(--border-radius);
-            }
-            &:hover{
-                position: relative;
-                z-index: 0;
-            }
-        }
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: -1px;
     }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 0;
+    }
+  }
+}
 </style>
