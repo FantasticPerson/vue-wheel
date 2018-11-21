1392a63ac8da95d8982fb1db22d8e23fd83b3289
wdd119 <1240172019@qq.com>
2018-11-21 20:51:44 +0800

    添加 input.vue 及 添加样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
+import Input from './input.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-group-button', ButtonGroup)
+Vue.component('g-input', Input)
+
 
 
 new Vue({
@@ -16,94 +19,3 @@ new Vue({
 })
 
 
-// import chai from 'chai'
-// import speies from 'chai-spies'
-
-// chai.use(speies)
-// const expect = chai.expect
-// //单元测试
-// try {
-//     {
-//         //测试按钮 含有 icon
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//             propsData: {
-//                 icon: 'setting'
-//             }
-//         })
-//         button.$mount('#test')
-//         let useElement = button.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-setting')
-
-//     }
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//             propsData: {
-//                 icon: 'setting',
-//                 loading: true
-//             }
-//         })
-//         button.$mount()
-//         let useElement = button.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-loading')
-//     }
-//     {
-//         const div = document.createElement('div')
-//         document.body.append(div)
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//             propsData: {
-//                 icon: 'setting'
-//             }
-//         })
-//         button.$mount(div)
-//         let svg = button.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('1')
-//         button.$el.remove()
-//         button.$destroy()
-//     }
-
-//     {
-//         const div = document.createElement('div')
-//         document.body.append(div)
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//             propsData: {
-//                 icon: 'setting',
-//                 iconPosition: 'right'
-//             }
-//         })
-//         button.$mount(div)
-
-//         let svg = button.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('2')
-//         button.$el.remove()
-//         button.$destroy()
-//     }
-//     {
-//         //mock
-//         const Constructor = Vue.extend(Button)
-//         const button = new Constructor({
-//             propsData: {
-//                 icon: 'setting',
-//                 iconPosition: 'right'
-//             }
-//         })
-//         button.$mount()
-//         let spy = chai.spy(function () { })
-//         button.$on('click', spy)
-//         button.$el.click()
-//         expect(spy).to.have.been.called()
-//     }
-// } catch (error) {
-//     window.errors = [error]
-// } finally {
-//     window.errors && window.errors.forEach((error) => {
-//         console.error(error.message)
-//     })
-// }
---
+++ src/input.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input @change="$emit('change',$event)" type="text" :value="value" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+            <icon name="error" class="icon-error"></icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
+    </div>    
+</template>
+<script>
+import Icon from './icon.vue'
+export default {
+    components:{Icon},
+    name:'GuluInput',
+    props:{
+        value:{
+            type:String
+        },
+        disabled:{
+            type:Boolean,
+            default:false
+        },
+        readonly:{
+            type:Boolean,
+            default:false
+        },
+        error:{
+            type:String
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$border-color-error:#F1453D;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items:center;
+  > *:not(:last-child) {
+      margin-right:0.5em;
+  }
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled],
+    &[readonly] {
+      border-color: #aaa;
+      color: #aaa;
+      cursor: not-allowed;
+    }
+  }
+  &.error {
+      > input{
+          border-color: $border-color-error;
+      }
+  }
+  .icon-error{
+      fill:$border-color-error;
+  }
+  .errorMessage{
+      color:$border-color-error;
+  }
+}
+</style>
