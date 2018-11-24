6c0883131b0987179b62647530f6513a282b4ee4
wdd119 <1240172019@qq.com>
2018-11-24 18:46:10 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
 import Row from './row.vue'
 import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Content from './content.vue'
+import Sider from './sider.vue'
+import Footer from './footer.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +17,13 @@ Vue.component('g-group-button', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-col', Col)
 Vue.component('g-row', Row)
+
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-sider', Sider)
+Vue.component('g-Footer', Footer)
+
 
 
 
