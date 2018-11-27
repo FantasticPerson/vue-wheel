25a0b4f55bc2098c16f93d91bcc3760f4a3575b9
wdd119 <1240172019@qq.com>
2018-11-27 20:22:18 +0800

    开始创建文档官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports ={
+    title:'gulu UI',
+    description:'Just applying around',
+    themeconfig:{
+        sidebar:[
+            '/',
+            '/install/'
+        ]
+    }
+    
+}
